66a67efe868705066c1cd70cf596827587344f2f
Hung-Kuang Han <hkh@Hung-KuangdeMacBook-Pro.local>
2021-12-13 17:03:59 -0800

    update readme

--- setup.sh
+++ setup.sh
@@ -14,7 +14,8 @@ git clone https://github.com/k2kobayashi/sprocket.git
 # create necessary paths
 SPROCKET_WAV=./sprocket/example/data/wav
 NSYNTH_ADUIO=./nsynth-train/audio
-mkdir -p $SPROCKET_WAV/{ACOUSTIC,ELECTRONIC}
+mkdir -p $SPROCKET_WAV/ACOUSTIC
+mkdir -p $SPROCKET_WAV/ELECTRONIC
 cp $NSYNTH_ADUIO/guitar_acoustic_017-*.wav $SPROCKET_WAV/ACOUSTIC
 cp $NSYNTH_ADUIO/guitar_electronic_017-*.wav $SPROCKET_WAV/ELECTRONIC
 
